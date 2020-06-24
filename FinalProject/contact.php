11ce381e68ced95fc67490af83e274fe317fc011
nic-lloyd <61630547+nic-lloyd@users.noreply.github.com>
2020-06-24 14:34:40 -0400

    Just fix CSS

---
+++ FinalProject/contact.php
@@ -0,0 +1,63 @@
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = "";
+$name = $email = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="styles.css">
+<title>plant practice</title>
+
+<link rel="stylesheet" href="styles.css">
+<style>
+    body {
+        color: white;
+    }
+
+    p {
+        text-align: center;
+    }
+
+    input {
+        color: black;
+    }
+</style>
+<p><span class="error">* required field</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  E-mail: <input type="text" name="email">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  <input type="submit" value="submit" name="submit">
+</form>
+<section class="home">
+        <a href="index.html">home</a>
+</section>
+
+<?php
+    mail($email, "Email to ".$name, "Hello, ".$name." thank you for using the plant practice app!");
+?>
